a855d4662c143620d467751ebeed87c591375806
Kyle Spraggs <theman@spiffyjr.me>
2012-05-25 16:40:28 -0500

    Added examples and updated module config to work out of the box.

--- config/module.config.php
+++ config/module.config.php
@@ -1,6 +1,14 @@
 <?php
 return array(
-    'ZfcTwig' => array(
+    'zfctwig' => array(
         'cache' => 'data/cache'
+    ),
+    'view_manager' => array(
+        'strategies'   => array('ViewTwigStrategy'),
+        'template_map' => array(
+            'index/index'   => __DIR__ . '/../view/index/index.twig',
+            'error/404'     => __DIR__ . '/../view/error/404.twig',
+            'error/index'   => __DIR__ . '/../view/error/index.twig',
+        ),
     )
 );
