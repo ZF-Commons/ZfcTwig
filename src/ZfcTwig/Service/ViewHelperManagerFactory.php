e1d01cc06965f2aa8d397a7c30957c3849302377
Roy van Kaathoven <royvankaathoven@hotmail.com>
2012-11-09 21:28:41 +0100

    Copied ViewHelperManager using Reflection

--- src/ZfcTwig/Service/ViewHelperManagerFactory.php
+++ src/ZfcTwig/Service/ViewHelperManagerFactory.php
@@ -4,22 +4,58 @@ namespace ZfcTwig\Service;
 
 use Zend\Mvc\Service\ViewHelperManagerFactory as ParentViewHelperManagerFactory;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\Stdlib\Hydrator\Reflection as ReflectionHydrator;
+use Zend\View\HelperPluginManager;
 
 /**
  * View Helper Manager Factory
  */
 class ViewHelperManagerFactory extends ParentViewHelperManagerFactory
 {
-    const PLUGIN_MANAGER_CLASS = 'ZfcTwig\View\HelperPluginManager';
+    /**
+     * @var \Zend\Stdlib\Hydrator\Reflection
+     */
+    protected $hydrator;
 
     /**
-     * @param \Zend\ServiceManager\ServiceLocatorInterface $serviceLocator
-     * @return \Zend\View\HelperPluginManager
+     * The properties which should be copied from the ViewHelperManager
+     * 
+     * @var array
+     */
+    protected $properties = array(
+        'invokableClasses',
+        'factories',
+        'abstractFactories',
+        'aliases',
+    );
+
+    /**
+     * @return Zend\Stdlib\Hydrator\Reflection
+     */
+    public function getHydrator()
+    {
+        if (null == $this->hydrator) {
+            $this->hydrator = new ReflectionHydrator;
+        }
+        
+        return $this->hydrator;
+    }
+
+    /**
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return HelperPluginManager
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
+        $hydrator = $this->getHydrator();
         $viewHelperManager = parent::createService($serviceLocator);
-        $viewHelperManager->addPeeringServiceManager($serviceLocator->get('ViewHelperManager'));
+        $copy = array();
+
+        $data = $hydrator->extract($serviceLocator->get('ViewHelperManager'));
+        foreach ($this->properties as $key) {
+            $copy[$key] = $data[$key];
+        }
+        $hydrator->hydrate($copy, $viewHelperManager);
         return $viewHelperManager;
     }
 }
